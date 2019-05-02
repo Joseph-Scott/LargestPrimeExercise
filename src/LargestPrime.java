b54bd060845636e420123136de4bc1f81c23d012
Joseph Scott <jbs198585@gmail.com>
2019-05-02 07:17:51 -0400

    solution

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -34,6 +34,15 @@ public class LargestPrime {
             return -1;
         }
 
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                number /= i;
+                i--;
+            }
+        }
+
+        return number;
+
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LargestPrime.getLargestPrime(217);
+        LargestPrime.getLargestPrime(35);
 
     }
 
